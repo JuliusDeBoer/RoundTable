fb65febd1040b367a056dee14c8cde00a800b53b
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2025-08-20 19:47:43 +0200

    feat: Add cool green box

--- src/components/app.rs
+++ src/components/app.rs
@@ -1,4 +1,4 @@
-use leptos::prelude::*;
+use leptos::{ev::MouseEvent, prelude::*};
 use wasm_bindgen::prelude::*;
 stylance::import_style!(style, "app.module.css");
 
@@ -10,9 +10,47 @@ extern "C" {
 
 #[component]
 pub fn App() -> impl IntoView {
+    let (pos, set_pos) = signal((0, 0));
+    let (start_pos, set_start_pos) = signal((0, 0));
+    let (pos_mod, set_pos_mod) = signal((0, 0));
+    let (is_pressed, set_is_pressed) = signal(false);
+
+    let on_down = move |e: MouseEvent| {
+        set_pos_mod.set((0, 0));
+        set_start_pos.set((e.screen_x(), e.screen_y()));
+        set_is_pressed.set(true);
+    };
+
+    let on_move = move |e: MouseEvent| {
+        set_pos_mod.set((e.screen_x(), e.screen_y()));
+    };
+
+    let on_up = move |_: MouseEvent| {
+        set_is_pressed.set(false);
+        set_pos.set((
+            pos.get().0 - (start_pos.get().0 - pos_mod.get().0),
+            pos.get().1 - (start_pos.get().1 - pos_mod.get().1),
+        ));
+    };
+
     view! {
-        <main class=style::container>
-            <h1>"Hello, World!"</h1>
-        </main>
+        <div
+            class=style::container
+            on:mousedown=on_down
+            on:mousemove=on_move
+            on:mouseup=on_up
+        >
+            <div
+                class=style::board
+                style:left=move || format!("{}px", match is_pressed.get() {
+                    false => pos.get().0,
+                    true => pos.get().0 - (start_pos.get().0 - pos_mod.get().0)
+                })
+                style:top=move || format!("{}px", match is_pressed.get() {
+                    false => pos.get().1,
+                    true => pos.get().1 - (start_pos.get().1 - pos_mod.get().1)
+                })
+                />
+        </div>
     }
 }
